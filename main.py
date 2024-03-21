d7c23a64d4bfb4ea8db5d069591170de59487334
Ed <eduramq@gmail.com>
2024-03-20 20:13:16 -0700

    Added memory to make multiple agent executor calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
 from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
@@ -28,17 +29,19 @@ prompt = ChatPromptTemplate(
                 "or what columns exist. Instead, use the 'describe_tables' function"
             )
         ),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 #  Initializing a table
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 
-agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
+agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools, memory=memory)
 
 # agent_executor("How many users have shipping address?")
 agent_executor("How many orders are there? Write the result to an html report.")
