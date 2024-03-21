5c327c1ff62f4b330693ab41615e6e570cd1f1cb
Ed <eduramq@gmail.com>
2024-03-20 18:58:05 -0700

    Added function to read tables and tables columns from db. Those are used as tools in agent instance

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+from langchain.schema import SystemMessage
 
-from tools.sql import run_query_tool
+from tools.sql import describe_tables_tool, list_tables, run_query_tool
 
 load_dotenv()
 
 
 chat = ChatOpenAI()
+tables = list_tables()
 
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(
+            content=f"You are an AI that has access to a SQLite database.\n{tables}"
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
+#  Initializing a table
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
-agent_executor("How many users are in the database?")
+agent_executor("How many users have shipping address?")
--- tools/sql.py
+++ tools/sql.py
@@ -6,12 +6,47 @@ from langchain.tools import Tool
 conn = sqlite3.connect("db.sqlite")
 
 
-def run_sqlite_query(query):
+def join_row_info_in_chatgpt_readable_form(rows):
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
+# Function that grabs table names from the sqlite db, it is used in a system message for the agent
+def list_tables():
+    """Function that list the name of tables in a sqlite database"""
     c = conn.cursor()
-    c.execute(query)
-    return c.fetchall()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = c.fetchall()
+    return join_row_info_in_chatgpt_readable_form(rows=rows)
+
+
+def run_sqlite_query(query):
+    """Function that runs a sql query created by a langchain Agent"""
+    c = conn.cursor()
+    try:
+        c.execute(query)
+        return c.fetchall()
+    except sqlite3.OperationalError as err:
+        return f"The following error occured: {str(err)}"
 
 
 run_query_tool = Tool.from_function(
     name="run_sqlite_query", description="Run a sqlite query.", func=run_sqlite_query
+)
+
+
+#
+def describe_tables(table_names):
+    """Function that describes the name of columns of tables in a database"""
+    c = conn.cursor()
+    tables = ", ".join("'" + table + "'" for table in table_names)
+    rows = c.execute(
+        f"SELECT sql FROM sqlite_master WHERE type='table' and NAME IN ({tables});"
+    )
+    return join_row_info_in_chatgpt_readable_form(rows=rows)
+
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given a list of table names, returns the schema",
+    func=describe_tables,
 )
